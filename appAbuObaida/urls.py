a4ed13463fa0d3046c1a4e7ca8473864b2426b1d
Consultant <samirsamado5@gmail.com>
2023-12-17 22:27:03 -0500

    question page

--- appAbuObaida/urls.py
+++ appAbuObaida/urls.py
@@ -4,4 +4,6 @@ from appAbuObaida import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('questions_blog/', views.questions_blog, name='questions_blog'),
+    path('question_detail/', views.question_detail, name='question_detail'),
 ]
--- appAbuObaida/views.py
+++ appAbuObaida/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 def index(request):
     return render(request, 'appAbuObaida/index.html')
+
+
+def questions_blog(request):
+    return render(request, 'appAbuObaida/questions_blog.html')
+
+
+def question_detail(request):
+    # question_detailed = get_object_or_404(Model, pk=question_detail)
+    context = {
+        # question_detailed: 'question_detailed'
+    }
+    return render(request, 'appAbuObaida/question_detail.html', context)
