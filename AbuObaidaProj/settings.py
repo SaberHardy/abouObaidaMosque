34c3ee0f55163f8b6002bacc35e0f88b15377cab
Consultant <samirsamado5@gmail.com>
2023-12-26 15:07:34 -0500

    allow hosts

--- AbuObaidaProj/settings.py
+++ AbuObaidaProj/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+i-4ru4*6ur-oe1#cd(z63)8(7&l(aqsjcrq6jhy^rk=upuq8u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "https://aoid-9e5d70135520.herokuapp.com/"]
 
 # Application definition
 
