cbc2120e9bad495c06127d5b8665198e528624a7
Consultant <samirsamado5@gmail.com>
2023-12-26 16:24:48 -0500

    media files

--- AbuObaidaProj/settings.py
+++ AbuObaidaProj/settings.py
@@ -121,8 +121,8 @@ STATICFILES_DIRS = [
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
