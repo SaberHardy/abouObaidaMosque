bea27146e735a1e067bd70fb7b5dfad7b2f38ad0
Consultant <samirsamado5@gmail.com>
2023-12-30 18:30:55 -0500

    problem with custom error page

--- AbuObaidaProj/settings.py
+++ AbuObaidaProj/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+i-4ru4*6ur-oe1#cd(z63)8(7&l(aqsjcrq6jhy^rk=upuq8u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'abou-obaida-f1bcab5996b8.herokuapp.com', 'www.aoidmosque.com']
 
 # Application definition
 
@@ -107,18 +107,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -127,3 +120,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
+
+SECURE_SSL_REDIRECT = False
